98f16e1f384a7f89c037c12446a84dc4c14f7cc1
mikejbe-9 <68225257+mikejbe-9@users.noreply.github.com>
2021-07-28 21:25:45 +0530

    Main menu added

---
+++ Shot Dead/Assets/Shot Dead/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Resolution[] resolutions;
+
+    public TextMeshProUGUI resolutionLabel;
+    public Toggle fullScreenTogg;
+    public Toggle vSyncTogg;
+
+    public int selectedResolution;
+    void Start()
+    {
+        fullScreenTogg.isOn = Screen.fullScreen;
+        if(QualitySettings.vSyncCount == 0)
+        {
+            vSyncTogg.isOn = false;
+        }
+        else
+        {
+            vSyncTogg.isOn = true;
+        }
+
+        bool foundRes = false;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+                selectedResolution = i;
+                UpdateResolutionText();
+            }
+        }
+        if (!foundRes)
+        {
+            resolutionLabel.text = Screen.width.ToString() + " X " + Screen.height.ToString();
+        }
+    }
+
+    public void DecreaseResolution()
+    {
+        selectedResolution--;
+
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+    }
+    public void IncreaseResolution()
+    {
+        selectedResolution++;
+
+        if(selectedResolution > resolutions.Length - 1)
+        {
+            selectedResolution = resolutions.Length - 1;
+        }
+    }
+    void UpdateResolutionText()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " X " + resolutions[selectedResolution].vertical.ToString();
+    }
+    public void ApplyGraphics()
+    {
+        if(vSyncTogg.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullScreenTogg.isOn);
+    }
+}
+
+[System.Serializable]
+public class Resolution
+{
+    public int horizontal;
+    public int vertical;
+}
---
+++ Shot Dead/Assets/Shot Dead/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Slider loadingBar;
+    public Text loadText;
+    public string scene;
+
+    public void LoadButton()
+    {
+        StartCoroutine(LoadScene(scene));
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        yield return null;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            loadingBar.value = asyncOperation.progress;
+            loadText.text = asyncOperation.progress * 100 + "%";
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
